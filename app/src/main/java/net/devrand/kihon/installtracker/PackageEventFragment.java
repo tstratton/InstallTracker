3272b9387a7614dc2dde19cc0fbf84557cb6e912
tstratton <tstratton@users.noreply.github.com>
2016-02-04 21:25:07 -0800

    launch app or app info for a package

--- app/src/main/java/net/devrand/kihon/installtracker/PackageEventFragment.java
+++ app/src/main/java/net/devrand/kihon/installtracker/PackageEventFragment.java
@@ -1,16 +1,19 @@
 package net.devrand.kihon.installtracker;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
@@ -21,14 +24,22 @@ import butterknife.ButterKnife;
 /**
  * Created by tstratto on 1/7/2016.
  */
-public class PackageEventFragment extends Fragment {
+public class PackageEventFragment extends Fragment implements View.OnClickListener {
 
     private static final String TAG = "PackageEventFragment";
 
     Cursor result;
     String packageName;
 
-    @Bind(R.id.list) ListView listView;
+    @Bind(R.id.list)
+    ListView listView;
+    @Bind(R.id.appinfo)
+    Button appInfo;
+    @Bind(R.id.applaunch)
+    Button appLaunch;
+
+    public PackageEventFragment() {
+    }
 
     public PackageEventFragment(String packageName) {
         this.packageName = packageName;
@@ -37,7 +48,7 @@ public class PackageEventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.event_list_fragment, container, false);
+        View root = inflater.inflate(R.layout.package_event_fragment, container, false);
         ButterKnife.bind(this, root);
 
         SQLiteOpenHelper db = DatabaseHelper.getInstance(getContext());
@@ -77,6 +88,9 @@ public class PackageEventFragment extends Fragment {
             }
         });
 
+        appInfo.setOnClickListener(this);
+        appLaunch.setOnClickListener(this);
+
         return root;
     }
 
@@ -99,4 +113,26 @@ public class PackageEventFragment extends Fragment {
         super.onStop();
     }
 
+    @Override
+    public void onClick(View view) {
+        String packageBase = packageName.startsWith("package:") ? packageName.substring("package:".length()) : packageName;
+        final PackageManager packageManager = view.getContext().getPackageManager();
+        Intent launchIntent = packageManager.getLaunchIntentForPackage(packageBase);
+        if (launchIntent == null) {
+            Toast.makeText(view.getContext(), "app not available", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        switch (view.getId()) {
+            case R.id.appinfo:
+                //redirect user to app Settings, from http://stackoverflow.com/a/17167502
+                Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse(packageName));
+                startActivity(intent);
+                break;
+            case R.id.applaunch:
+                startActivity(launchIntent);
+                break;
+        }
+    }
 }
