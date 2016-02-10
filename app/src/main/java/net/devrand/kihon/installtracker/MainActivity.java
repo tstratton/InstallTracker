99159cd788439c17ae4bf5f01c75bc9d1d0094d3
tstratton <tstratton@users.noreply.github.com>
2016-02-09 21:59:04 -0800

    move export to memu item

--- app/src/main/java/net/devrand/kihon/installtracker/MainActivity.java
+++ app/src/main/java/net/devrand/kihon/installtracker/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -121,6 +123,27 @@ public class MainActivity extends AppCompatActivity {
                 new ExportTask(exportButton).execute();
             }
         });
+    }
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_export:
+                new ExportTask(exportButton).execute();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     static final String jsonFileName = "/sdcard/installTracker-export.json";
